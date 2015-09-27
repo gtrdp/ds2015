38e7d4fe208e4c5c71a3810607865e95003bcef9
Guntur Dharma Putra <guntur.dharma@gmail.com>
2015-09-27 17:46:02 +0200

    first working bully alg for 2 processes

--- server/server.js
+++ server/server.js
@@ -15,32 +15,29 @@ var serverStarted = false;
 var currentPort = 0;
 var leaderPort = 0;
 
-// function declaration
-function haltOnTimedout(req, res, next){
-	leaderPort = currentPort;
-  	console.log('I\'m the leader!');
-}
-
 function createHTTPServer(portNumber){
-	// app.use(timeout(3000));
-	// app.use(haltOnTimedout);
 	app.use(bodyParser.urlencoded({ extended: false }));
 
 	app.post('/',function(req,res){
 	  var message = req.body.message;
-	  console.log(req);
+	  var from = req.body.from;
+	  
 	  if (message == 'ok') {
 	  	console.log('got a response');
 	  } else if (message == 'election') {
-	  	console.log('got a election request');
+	  	console.log('Got a election request from ' + from);
+	  	startElection();
+	  } else if (message == 'leader') {
+	  	leaderPort = from;
+	  	console.log('The leader is now' + from);
 	  } else {
 	  	console.log(message);
 	  }
-	  // res.end("");
+	  res.end('ok');
 	});
 
 	app.listen(portNumber,function(){
-	  console.log('Server running at http://127.0.0.1:/' + portNumber);
+	  console.log('Server running at http://127.0.0.1:/' + portNumber + '\n');
 
 	  startElection();
 	});
@@ -49,46 +46,30 @@ function createHTTPServer(portNumber){
 function startElection(){
 	console.log('Initiating election...');
 	console.log('Sending election request to ' + (currentPort+1) + '...');
-	var http = require('http');
 
-	var options = {
-	  host: '127.0.0.1',
-	  path: '/',
-	  port: currentPort + 1,
-	  method: 'POST',
-	  form: {'message': 'election', 'from': currentPort}
-	};
+	var request = require('request');
 
-	var req = http.request(options, function(response) {
-	  var str = ''
-	  response.on('data', function (chunk) {
-	    str += chunk;
-	  });
+	request.post(
+	    'http://127.0.0.1:' + (currentPort+1),
+	    { form: { message: 'election', from: currentPort }},
+	    function (error, response, body) {
+	    	if(!error) {
+	    		console.log(body);
+	    	}else if (error.code === 'ECONNREFUSED') {
+	            console.log('No response from ' + (currentPort+1) + '.');
 
-	  response.on('end', function () {
-	    console.log(str);
-	  });
-	});
-
-	req.on('socket', function (socket) {
-	    socket.setTimeout(3000);  
-	    socket.on('timeout', function() {
-	        req.abort();
-	        console.log('I\'m the leader!');
-	    });
-	});
-
-	req.on('error', function(err) {
-		if (err.errno == 'ECONNREFUSED') {
-			console.log('Port ' +(currentPort+1)+ ' is closed.');
-			console.log('I\'m the leader!');
-		}
-		req.end();
-	});
-
-	req.write("");
-	req.end();
+	            leaderPort = currentPort;
+	            console.log('I\'m the Leader! \n');
+	        } else {
+	        	console.log(body);
+	        }
+	    }
+	);
 };
+
+function broadcastMsg() {
+
+}
 
 
 /**
