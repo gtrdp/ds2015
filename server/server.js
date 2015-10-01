6a3ef7bf3a2cac9d48e1135a29e2a89e1fe15403
Guntur Dharma Putra <guntur.dharma@gmail.com>
2015-10-01 22:27:28 +0200

    First working server with unicast leader election in TCP socket

--- server/server.js
+++ server/server.js
@@ -6,71 +6,88 @@
  * 	- body-parser
  */
 // global vars
-var express		= require("express");
-var bodyParser 	= require("body-parser");
-var timeout 	= require('connect-timeout');
 var portscanner = require('portscanner');
-var app			= express();
 var serverStarted = false;
 var currentPort = 0;
 var leaderPort = 0;
 
-function createHTTPServer(portNumber){
-	app.use(bodyParser.urlencoded({ extended: false }));
+var net = require('net');
+var server;
 
-	app.post('/',function(req,res){
-	  var message = req.body.message;
-	  var from = req.body.from;
-	  
-	  if (message == 'ok') {
-	  	console.log('got a response');
-	  } else if (message == 'election') {
-	  	console.log('Got a election request from ' + from);
-	  	startElection();
-	  } else if (message == 'leader') {
-	  	leaderPort = from;
-	  	console.log('The leader is now' + from);
-	  } else {
-	  	console.log(message);
-	  }
-	  res.end('ok');
-	});
-
-	app.listen(portNumber,function(){
-	  console.log('Server running at http://127.0.0.1:/' + portNumber + '\n');
-
-	  startElection();
-	});
-};
-
-function startElection(){
-	console.log('Initiating election...');
-	console.log('Sending election request to ' + (currentPort+1) + '...');
-
-	var request = require('request');
-
-	request.post(
-	    'http://127.0.0.1:' + (currentPort+1),
-	    { form: { message: 'election', from: currentPort }},
-	    function (error, response, body) {
-	    	if(!error) {
-	    		console.log(body);
-	    	}else if (error.code === 'ECONNREFUSED') {
-	            console.log('No response from ' + (currentPort+1) + '.');
-
-	            leaderPort = currentPort;
-	            console.log('I\'m the Leader! \n');
-	        } else {
-	        	console.log(body);
-	        }
-	    }
-	);
-};
-
-function broadcastMsg() {
+function multiMsg(message, from, to) {
 
 }
 
+function createServer(portNumber) {
+	server = net.createServer(function(socket) {
+		socket.on('data', function(data) {
+			var JSONData = JSON.parse(data.toString());
+
+			var message = JSONData.message;
+			var from = JSONData.from;
+
+			if (message == 'ok') {
+				console.log('got a response');
+			} else if (message == 'election') {
+				console.log('Got a election request from ' + from);
+				socket.write('ok');
+
+				startElectionTCP(portNumber);
+			} else if (message == 'leader') {
+				leaderPort = from;
+				console.log('The leader is now' + from);
+			} else {
+				console.log(message);
+			}
+		});
+
+		socket.on('close', function() {
+			// console.log('Close the connection');
+		});
+
+		socket.on('error', function(e) {
+			console.log('error ', e);
+		});
+
+		socket.pipe(socket);
+	}).listen(portNumber, '127.0.0.1');
+
+	console.log('Server is running at http://127.0.0.1:' + portNumber + '\n');
+
+	startElectionTCP(portNumber);
+}
+
+function startElectionTCP(ourPort){
+	console.log('Initiating election...');
+	console.log('Sending election request to ' + (ourPort+1) + '...');
+
+	var destinationPort = ourPort + 1;
+	var client = new net.Socket();
+	client.connect(destinationPort, '127.0.0.1', function() {
+		client.write('{ "message": "election", "from": "'+currentPort+'" }');
+	});
+
+	client.on('data', function(data) {
+		if(data.toString() == 'ok') {
+			console.log('Server ' +destinationPort+ ' will take over.');
+		} else {
+			console.log('No response from ' + (currentPort+1) + '.');
+
+            leaderPort = currentPort;
+            console.log('I\'m the Leader! \n');
+		}
+		client.destroy();
+	});
+
+	client.on('error', function(error) {
+		if (error.code === 'ECONNREFUSED') {
+            console.log('ECONNREFUSED. Server running on ' + (currentPort+1) + ' is down.');
+
+            leaderPort = currentPort;
+            console.log('I\'m the Leader! \n');
+        }
+	});
+};
 
 /**
  * Starts here.
@@ -81,7 +98,7 @@ console.log('Scan available port...');
 
 portscanner.findAPortNotInUse(8081, 3010, '127.0.0.1', function(error, port) {
 	console.log('Port ' + port + ' is open.');
-	createHTTPServer(port);
+	createServer(port);
 	currentPort = port;
 	serverStarted = true;
 });
